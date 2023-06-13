064bf3045756f229e1d41f27c240b05eabe63675
vikul rathod <vikul.rathod.net@gmail.com>
2023-06-13 09:38:40 +0530

    added add method with a new Calculator class

---
+++ DemoSolution/First/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"Addition: {a} + {b} = {a + b}");
+        }
+    }
+}
